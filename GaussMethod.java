0adbce490a359e1691f00474bdb5c284678c5536
Батомункуева Виктория <torichan373737@gmail.com>
2023-02-13 13:36:04 +0300

    перестановка строк и опеределитель для квадратной матрицы

--- GaussMethod.java
+++ GaussMethod.java
@@ -9,6 +9,15 @@ public class GaussMethod {
         int cols = matrix[0].length;
 
         for (int k = 0; k < rows; k++) {
+            System.out.println("k: " + k);
+            System.out.println("matrix[k][k]: " + matrix[k][k]);
+            if (matrix[k][k] == 0) {
+                System.out.println("it's 0\n___________________");
+                switch_rows(matrix, k);
+            }
+            System.out.println("___________________");
+
+
             for (int i = 0; i < rows; i++) {
                 float to_mul = -matrix[i][k] / matrix[k][k];
                 for (int j = 0; j < cols; j++) {
@@ -23,23 +32,48 @@ public class GaussMethod {
 //                    System.out.println("we get " + matrix[i][j] + "\n_____________");
                 }
 //                System.out.println("______________________________");
+
+
+                System.out.println("_______________________________");
+                for (int l = 0; l < rows; l++) {
+                    for (int m = 0; m < cols; m++) {
+                        System.out.println(matrix[l][m]);
+                    }
+                }
             }
         }
 
-        System.out.println("_______________________________");
-        for (int l = 0; l < rows; l++) {
-            for (int m = 0; m < cols; m++) {
-                System.out.println(matrix[l][m]);
+
+        float det = get_determinant(matrix);
+        System.out.println("Det = " + det);
+    }
+
+    // пока для квадратной матрицы
+    float get_determinant(float[][] matrix){
+        float det = 1;
+        for (int i = 0; i < matrix.length; i++) {
+            det *= matrix[i][i];
+        }
+        return det;
+    }
+
+    void switch_rows(float[][] matrix, int i) {
+        int c = 0;
+        System.out.println("new call\ni = " + i);
+        while (matrix[i + c][i] == 0) {
+            if (i + c == matrix.length - 1) {
+                c = -i;
+                System.out.println("new cycle");
+            } else {
+                System.out.println("to switch with: " + matrix[i + c][i]);
+                c++;
             }
         }
-    }
-
-
-
-
-
-    void switch_rows(float[][] matrix){
+        for (int j = 0; j < matrix[i].length; j++) {
+            float temp = matrix[i + c][j];
+            matrix[i + c][j] = matrix[i][j];
+            matrix[i][j] = temp;
+        }
 
     }
-
 }
--- Main.java
+++ Main.java
@@ -1,13 +1,25 @@
 public class Main {
     //Variant 1
-    public static float[][] matrix = {{2, -3, 2, 1}, {1, 1, 1, 3}, {1, 1, -2, 0}};
+//    public static float[][] matrix = {{0, 1, 2, 3}, {4, 0, 5, 6}, {7, 8, 0, 9}};
+    public static float[][] matrix = {{-7, 8, 9}, {1, -2, 3}, {4, 0, 6}};
 
     //        float[][] a = {{1, 1, 2, -1, 9}, {1, 1, -2, 1, 7}, {1, -1, 1, 2, -9}, {1, -1, -1, -2, 5}};
 //        float[][] a = {{1, 1, 2, 3, 1}, {1, 2, 3, -1, -4}, {3, -1, -1, -2, -4}, {2, 3, -1, -1, -6}};
     public static void main(String[] args) {
+
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
         GaussMethod gaussMethod = new GaussMethod();
 
         gaussMethod.to_triangle(matrix);
+
+        System.out.println("_______________________________");
+        for (int l = 0; l < rows; l++) {
+            for (int m = 0; m < cols; m++) {
+                System.out.println(matrix[l][m]);
+            }
+        }
     }
 
 
