3c0609e0a7a5792ad4b1625d0ef19df262c731e8
Батомункуева Виктория <torichan373737@gmail.com>
2023-03-06 14:41:36 +0300

    поиск решения матрицы

--- GaussMethod.java
+++ GaussMethod.java
@@ -9,13 +9,13 @@ public class GaussMethod {
         int cols = matrix[0].length;
 
         for (int k = 0; k < rows; k++) {
-            System.out.println("k: " + k);
-            System.out.println("matrix[k][k]: " + matrix[k][k]);
+//            System.out.println("k: " + k);
+//            System.out.println("matrix[k][k]: " + matrix[k][k]);
             if (matrix[k][k] == 0) {
-                System.out.println("it's 0\n___________________");
+//                System.out.println("it's 0\n___________________");
                 switch_rows(matrix, k);
             }
-            System.out.println("___________________");
+//            System.out.println("___________________");
 
 
             for (int i = 0; i < rows; i++) {
@@ -34,10 +34,10 @@ public class GaussMethod {
 //                System.out.println("______________________________");
 
 
-                System.out.println("_______________________________");
+//                System.out.println("_______________________________");
                 for (int l = 0; l < rows; l++) {
                     for (int m = 0; m < cols; m++) {
-                        System.out.println(matrix[l][m]);
+//                        System.out.println(matrix[l][m]);
                     }
                 }
             }
@@ -45,11 +45,11 @@ public class GaussMethod {
 
 
         float det = get_determinant(matrix);
-        System.out.println("Det = " + det);
+//        System.out.println("Det = " + det);
     }
 
     // пока для квадратной матрицы
-    float get_determinant(float[][] matrix){
+    float get_determinant(float[][] matrix) {
         float det = 1;
         for (int i = 0; i < matrix.length; i++) {
             det *= matrix[i][i];
@@ -59,13 +59,13 @@ public class GaussMethod {
 
     void switch_rows(float[][] matrix, int i) {
         int c = 0;
-        System.out.println("new call\ni = " + i);
+//        System.out.println("new call\ni = " + i);
         while (matrix[i + c][i] == 0) {
             if (i + c == matrix.length - 1) {
                 c = -i;
-                System.out.println("new cycle");
+//                System.out.println("new cycle");
             } else {
-                System.out.println("to switch with: " + matrix[i + c][i]);
+//                System.out.println("to switch with: " + matrix[i + c][i]);
                 c++;
             }
         }
@@ -74,6 +74,55 @@ public class GaussMethod {
             matrix[i + c][j] = matrix[i][j];
             matrix[i][j] = temp;
         }
+    }
 
+    float[] getSolution(float[][] matrix) {
+        int rows = matrix.length - 1;
+        int cols = matrix[0].length - 1;
+        System.out.println("rows = " + rows);
+        System.out.println("cols = " + cols);
+        float[] solutions = new float[cols];
+
+        for (int i = rows; i >= 0; --i) {
+            for (int j = cols - 1; j >= 0; --j) {
+                System.out.println("j = " + j);
+                System.out.println("i = " + i);
+//                float temp = 0;
+//                if (j == cols - 1) {
+//                    temp = matrix[i][j + 1] / matrix[i][j];
+//                    solutions[j] = temp;
+//                    System.out.println("matrix[i][j + 1] = " + matrix[i][j + 1]);
+//                    System.out.println("matrix[i][j] = " + matrix[i][j]);
+//                    System.out.println("temp = " + temp);
+//                    i--;
+//                } else if (j == cols - 2) {
+//                    temp = (matrix[i][j + 2] - solutions[j + 1] * matrix[i][j + 1]) / matrix[i][j];
+//                    solutions[j] = temp;
+//                    System.out.println("matrix[i][j + 2] = " + matrix[i][j + 2]);
+//                    System.out.println("matrix[i][j + 1] = " + matrix[i][j + 1]);
+//                    System.out.println("matrix[i][j] = " + matrix[i][j]);
+//                    System.out.println("temp = " + temp);
+//                    i--;
+//                } else if (j == cols - 3) {
+//                    temp = (matrix[i][j + 3] - solutions[j + 2] * matrix[i][j + 2] - solutions[j + 1] * matrix[i][j + 1]) / matrix[i][j];
+//                    solutions[j] = temp;
+//                    System.out.println("matrix[i][j + 3] = " + matrix[i][j + 3]);
+//                    System.out.println("matrix[i][j + 2] = " + matrix[i][j + 2]);
+//                    System.out.println("matrix[i][j + 1] = " + matrix[i][j + 1]);
+//                    System.out.println("matrix[i][j] = " + matrix[i][j]);
+//                    System.out.println("temp = " + temp);
+//                    i--;
+//                }
+
+                float to_sub = 0;
+                for (int k = j; k < cols; ++k) {
+                    to_sub += solutions[k] * matrix[i][k];
+                }
+                solutions[j] = (matrix[i][cols] - to_sub) / matrix[i][j];
+                System.out.println("temp = " + solutions[j]);
+                i--;
+            }
+        }
+        return solutions;
     }
 }
--- Main.java
+++ Main.java
@@ -1,7 +1,8 @@
 public class Main {
     //Variant 1
 //    public static float[][] matrix = {{0, 1, 2, 3}, {4, 0, 5, 6}, {7, 8, 0, 9}};
-    public static float[][] matrix = {{-7, 8, 9}, {1, -2, 3}, {4, 0, 6}};
+//    public static float[][] matrix = {{-7, 8, 9}, {1, -2, 3}, {4, 0, 6}};
+    public static float[][] matrix = {{2, -3, 2, 1}, {1, 1, 1, 3}, {1, 1, -2, 0}};
 
     //        float[][] a = {{1, 1, 2, -1, 9}, {1, 1, -2, 1, 7}, {1, -1, 1, 2, -9}, {1, -1, -1, -2, 5}};
 //        float[][] a = {{1, 1, 2, 3, 1}, {1, 2, 3, -1, -4}, {3, -1, -1, -2, -4}, {2, 3, -1, -1, -6}};
@@ -14,11 +15,19 @@ public class Main {
 
         gaussMethod.to_triangle(matrix);
 
-        System.out.println("_______________________________");
-        for (int l = 0; l < rows; l++) {
-            for (int m = 0; m < cols; m++) {
-                System.out.println(matrix[l][m]);
-            }
+//        System.out.println("_______________________________");
+//        for (int l = 0; l < rows; l++) {
+//            for (int m = 0; m < cols; m++) {
+//                System.out.println(matrix[l][m]);
+//            }
+//        }
+
+
+
+        float[] result = gaussMethod.getSolution(matrix);
+        System.out.println("___________________________\n");
+        for (float i : result) {
+            System.out.println(i);
         }
     }
 
