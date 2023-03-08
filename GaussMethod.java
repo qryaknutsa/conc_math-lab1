43be83a8d2319d991abde6f953b0d1712b7d310c
Батомункуева Виктория <torichan373737@gmail.com>
2023-03-08 16:46:30 +0300

    все случаи рассмотрены

--- GaussMethod.java
+++ GaussMethod.java
@@ -1,51 +1,48 @@
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
+
+import static java.lang.Float.*;
 
 public class GaussMethod {
-
-    int determinant_sign = 1;
-    // matrix = {{2, -3, 2, 1}, {1, 1, 1, 3}, {1, 1, -2, 0}};
 
     public void to_triangle(float[][] matrix) {
         int rows = matrix.length;
         int cols = matrix[0].length;
 
         for (int k = 0; k < rows; k++) {
-//            System.out.println("k: " + k);
-//            System.out.println("matrix[k][k]: " + matrix[k][k]);
+            System.out.println("k: " + k);
+            System.out.println("matrix[k][k]: " + matrix[k][k]);
+            boolean isReplacement = true;
             if (matrix[k][k] == 0) {
-//                System.out.println("it's 0\n___________________");
-                switch_rows(matrix, k);
+                System.out.println("it's 0\n___________________");
+                isReplacement = switch_rows(matrix, k);
             }
-//            System.out.println("___________________");
+            System.out.println("___________________");
 
+            if (!isReplacement) {
+                return;
+            }
 
             for (int i = 0; i < rows; i++) {
-                float to_mul = -matrix[i][k] / matrix[k][k];
+                float to_mul;
+                if (matrix[i][k] == 0) to_mul = 0;
+                else to_mul = -matrix[i][k] / matrix[k][k];
                 for (int j = 0; j < cols; j++) {
                     if (i <= k || j < k) {
-//                        System.out.println("Continue");
+                        System.out.println("Continue");
                         continue;
                     }
-//                    System.out.println("_____________\nfor [" + i + "," + j + "]\nto_mul= " + to_mul);
-//                    System.out.println("a[k][j] = " + matrix[k][j]);
-//                    System.out.println("a[i][j] = " + matrix[i][j]);
+                    System.out.println("_____________\nfor [" + i + "," + j + "]\nto_mul= " + to_mul);
+                    System.out.println("a[k][j] = " + matrix[k][j]);
+                    System.out.println("a[i][j] = " + matrix[i][j]);
                     matrix[i][j] = matrix[i][j] + matrix[k][j] * to_mul;
-//                    System.out.println("we get " + matrix[i][j] + "\n_____________");
+                    System.out.println("we get " + matrix[i][j] + "\n_____________");
                 }
-//                System.out.println("______________________________");
+                System.out.println("______________________________");
 
-
-//                System.out.println("_______________________________");
-                for (int l = 0; l < rows; l++) {
-                    for (int m = 0; m < cols; m++) {
-//                        System.out.println(matrix[l][m]);
-                    }
-                }
             }
         }
-
-
-        float det = get_determinant(matrix);
-//        System.out.println("Det = " + det);
     }
 
     // пока для квадратной матрицы
@@ -54,21 +51,24 @@ public class GaussMethod {
         for (int i = 0; i < matrix.length; i++) {
             det *= matrix[i][i];
         }
+        if (det == -0.0) det = 0;
         return det;
     }
 
-    void switch_rows(float[][] matrix, int i) {
+    boolean switch_rows(float[][] matrix, int i) {
         int c = 0;
-//        System.out.println("new call\ni = " + i);
+        int isInf = 0;
         while (matrix[i + c][i] == 0) {
-            if (i + c == matrix.length - 1) {
-                c = -i;
-//                System.out.println("new cycle");
-            } else {
-//                System.out.println("to switch with: " + matrix[i + c][i]);
-                c++;
-            }
+            if (isInf > matrix.length + 2) return false;
+            int temp = i + c == matrix.length - 1 ? c = -i : c++;
+            isInf++;
         }
+        if (c != -1) shift_rows(i,c,matrix);
+        return true;
+    }
+
+
+    void shift_rows(int i, int c, float[][] matrix) {
         for (int j = 0; j < matrix[i].length; j++) {
             float temp = matrix[i + c][j];
             matrix[i + c][j] = matrix[i][j];
@@ -76,50 +76,38 @@ public class GaussMethod {
         }
     }
 
+    int checkRang(float[][] matrix) {
+        int n = matrix.length;
+        int zero_sum = matrix.length;
+        int zero_sum_b = matrix.length;
+        for (int i = 0; i < matrix.length; i++) {
+            int sum = 0;
+            for (int j = 0; j < matrix.length; j++) {
+                if (matrix[i][j] == 0) sum++;
+            }
+            if (sum == n) zero_sum--;
+            if (sum == n && matrix[i][n] == 0) zero_sum_b--;
+        }
+        if (zero_sum != zero_sum_b) return 0;
+        else if (zero_sum == zero_sum_b && zero_sum < n) return 2;
+        else return 1;
+    }
+
     float[] getSolution(float[][] matrix) {
+        MathContext context = new MathContext(20, RoundingMode.HALF_UP);
         int rows = matrix.length - 1;
         int cols = matrix[0].length - 1;
-        System.out.println("rows = " + rows);
-        System.out.println("cols = " + cols);
         float[] solutions = new float[cols];
-
         for (int i = rows; i >= 0; --i) {
             for (int j = cols - 1; j >= 0; --j) {
-                System.out.println("j = " + j);
-                System.out.println("i = " + i);
-//                float temp = 0;
-//                if (j == cols - 1) {
-//                    temp = matrix[i][j + 1] / matrix[i][j];
-//                    solutions[j] = temp;
-//                    System.out.println("matrix[i][j + 1] = " + matrix[i][j + 1]);
-//                    System.out.println("matrix[i][j] = " + matrix[i][j]);
-//                    System.out.println("temp = " + temp);
-//                    i--;
-//                } else if (j == cols - 2) {
-//                    temp = (matrix[i][j + 2] - solutions[j + 1] * matrix[i][j + 1]) / matrix[i][j];
-//                    solutions[j] = temp;
-//                    System.out.println("matrix[i][j + 2] = " + matrix[i][j + 2]);
-//                    System.out.println("matrix[i][j + 1] = " + matrix[i][j + 1]);
-//                    System.out.println("matrix[i][j] = " + matrix[i][j]);
-//                    System.out.println("temp = " + temp);
-//                    i--;
-//                } else if (j == cols - 3) {
-//                    temp = (matrix[i][j + 3] - solutions[j + 2] * matrix[i][j + 2] - solutions[j + 1] * matrix[i][j + 1]) / matrix[i][j];
-//                    solutions[j] = temp;
-//                    System.out.println("matrix[i][j + 3] = " + matrix[i][j + 3]);
-//                    System.out.println("matrix[i][j + 2] = " + matrix[i][j + 2]);
-//                    System.out.println("matrix[i][j + 1] = " + matrix[i][j + 1]);
-//                    System.out.println("matrix[i][j] = " + matrix[i][j]);
-//                    System.out.println("temp = " + temp);
-//                    i--;
-//                }
-
                 float to_sub = 0;
                 for (int k = j; k < cols; ++k) {
                     to_sub += solutions[k] * matrix[i][k];
+//                    System.out.println("solutions[k] = " + solutions[k]);
+//                    System.out.println("matrix[i][k] = " + matrix[i][k]);
                 }
-                solutions[j] = (matrix[i][cols] - to_sub) / matrix[i][j];
-                System.out.println("temp = " + solutions[j]);
+                solutions[j] = new BigDecimal((matrix[i][cols] - to_sub) / matrix[i][j], context).floatValue();
+//                System.out.println("temp = " + solutions[j]);
                 i--;
             }
         }
